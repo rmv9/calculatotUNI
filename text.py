e5c53706e59351969d2ad1da72045531169d282c
Maxim Razdorozhny <rmv_9@mail.ru>
2024-02-13 09:48:21 +0300

    Del some files, modify README

--- text.py
+++ text.py
@@ -3,24 +3,6 @@
 # CALCULATOR_BETA v.1
 # copy_past line
 
-# update list
-
-# float and minus (-7, -14, -0.5) operands added                        1.11
-# round answer numbers                                                  1.12
-# number.strip() method                                                 1.13
-# convert lists to dictionary
-# change cycles user_number_1 -> cycle_num_1 or else
-# debug space-inter error                                               1.14
-# txt_list pack added
-# animated intro added                                                  1.15
-#
-# todo list
-# 1. dev interface
-# 2. add list modul backend
-# 3. add math module backend - ???
-
-
-# words and symbols list for calculator_beta
 
 prog_name = 'калькулятор'
 ver = 'v1.15'
